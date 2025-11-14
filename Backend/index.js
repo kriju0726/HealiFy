770c63f1066bdc1ffa690ab355243c7c664dce7c
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2025-11-14 11:49:27 +0530

    Active Backend Part

--- Backend/index.js
+++ Backend/index.js
@@ -6,20 +6,16 @@ import connectDb from "./config/db.js";
 dotenv.config();
 const app = express();
 
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     credentials: true
-// }))
-
+app.use(cors());
 app.use(express.json());
 
+// ADD THIS ROUTE
+app.get("/", (req, res) => {
+    res.send("HealiFy Backend is Running Successfully 🚀");
+});
 
-
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     connectDb();
     console.log(`Server is running on port ${port}`);
 });
-
-
-
